1b7e76236aa40a4ee7bdfdb8567ab71c46c16a57
Rafaroden <rafaellaroden@gmail.com>
2025-03-21 19:55:40 -0300

    feat: create page configuration and style

---
+++ src/pages/configuration/index.tsx
@@ -0,0 +1,12 @@
+// ./ busca o caminho da pasta 
+
+import { StyledConfigurationPage } from "./style"  
+export const Configuration = () => {
+    return (
+        <StyledConfigurationPage>
+            {/* // colocar aqui o html */}
+        </StyledConfigurationPage>
+        
+    )
+}
+
